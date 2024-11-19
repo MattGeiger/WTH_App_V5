dc3fcca2c3e6cf6683958a901811e64e78c25653
MattGeiger <4166270+MattGeiger@users.noreply.github.com>
2024-11-19 05:38:01 +0000

    Changes to be committed: 	modified:   backend/database.db 	modified:   backend/index.js

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ app.use(express.static('public'));
 app.get('/data', async (req, res) => {
   try {
     const db = await dbPromise;
-    const rows = await db.all('SELECT * FROM food_items');
+    const rows = await db.all('SELECT * FROM food_items WHERE english_category = ?', ['Beans']);
     res.json({ data: rows });
   } catch (error) {
     res.status(500).json({ error: error.message });
